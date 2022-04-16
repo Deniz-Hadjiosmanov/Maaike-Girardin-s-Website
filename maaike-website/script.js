46b94cf13164bca214ef0b83518bd7ec289a23a6
Nikoleta Dimitrova <n.dimitrova@student.fontys.nl>
2022-04-16 21:29:38 +0200

    Adding interactive tree

--- maaike-website/script.js
+++ maaike-website/script.js
@@ -6,3 +6,11 @@ function submitForm() {
     window.location.reload();
 }
 
+document.getElementById("myVid").addEventListener("mouseover", function() {
+	this.play();
+});
+
+document.getElementById("myVid").addEventListener("mouseleave", function() {
+	this.pause();
+});
+
