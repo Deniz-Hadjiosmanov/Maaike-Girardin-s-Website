a9ca2d6ec3f1da605f01447be4be1b7287fc6602
Nikoleta Dimitrova <n.dimitrova@student.fontys.nl>
2022-05-05 21:40:07 +0200

    Adding the contact page

--- maaike-website/script.js
+++ maaike-website/script.js
@@ -1,3 +1,18 @@
+let fields = document.querySelectorAll('.field');
+
+for (let i = 0; i < fields.length; i++) {
+	fields[i].firstElementChild.addEventListener('focus', () => {
+		fields[i].querySelector('label').classList.add('top');
+		fields[i].querySelector('.border').style.transform = 'scale(1)';
+	});
+	fields[i].firstElementChild.addEventListener('blur', () => {
+		if (fields[i].firstElementChild.value.length <= 0) {
+			fields[i].querySelector('label').classList.remove('top');
+		}
+		fields[i].querySelector('.border').style.transform = 'scale(0)';
+	});
+}
+
 function openMobileMenu(menu) {
     menu.classList.toggle('open');
 }
@@ -9,5 +24,4 @@ function submitForm() {
 document.getElementById("myVid").addEventListener("mouseover", function() {
 	this.play();
 });
-
 
