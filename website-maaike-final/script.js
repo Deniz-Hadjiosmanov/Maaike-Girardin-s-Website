a409e089e101b14bc0d9b5d9ea9a25ee7216424a
Nikoleta Dimitrova <n.dimitrova@student.fontys.nl>
2022-05-16 12:02:47 +0200

    Adding contact me page

---
+++ website-maaike-final/script.js
@@ -0,0 +1,21 @@
+/* Contact me form input interaction */
+let fields = document.querySelectorAll('.field');
+
+for (let i = 0; i < fields.length; i++) {
+	fields[i].firstElementChild.addEventListener('focus', () => {
+		fields[i].querySelector('label').classList.add('top');
+		fields[i].querySelector('.border').style.transform = 'scale(1)';
+	});
+	fields[i].firstElementChild.addEventListener('blur', () => {
+		if (fields[i].firstElementChild.value.length <= 0) {
+			fields[i].querySelector('label').classList.remove('top');
+		}
+		fields[i].querySelector('.border').style.transform = 'scale(0)';
+	});
+}
+
+/* Tree video play on hover */
+document.getElementById("myVid").addEventListener("mouseover", function() {
+	this.play();
+});
+
