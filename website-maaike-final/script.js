c338e1984e6a1b3ea2e683f2a785798b617a6046
Nikoleta Dimitrova <n.dimitrova@student.fontys.nl>
2022-05-17 12:54:28 +0200

    Adding first version of pop up

--- website-maaike-final/script.js
+++ website-maaike-final/script.js
@@ -1,6 +1,13 @@
 /* Contact me form input interaction */
 let fields = document.querySelectorAll('.field');
 
+const form = document.getElementById('contact-form');
+form.addEventListener("submit", () => {
+		form.submit();
+		alert("Thank you for your interest in my work! Your email was sent.");
+
+})
+
 for (let i = 0; i < fields.length; i++) {
 	fields[i].firstElementChild.addEventListener('focus', () => {
 		fields[i].querySelector('label').classList.add('top');
@@ -15,7 +22,9 @@ for (let i = 0; i < fields.length; i++) {
 }
 
 /* Tree video play on hover */
-document.getElementById("myVid").addEventListener("mouseover", function() {
+document.getElementById("myVid").addEventListener("mouseover", function () {
 	this.play();
 });
+
+
 
